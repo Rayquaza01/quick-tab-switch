957d8433076f89665b447c7aa1f1500d17353b43
Joe Jarvis <joe.jarvis109@gmail.com>
2019-07-31 21:30:32 -0400

    More complete

--- extension/index.js
+++ extension/index.js
@@ -6,14 +6,14 @@ function getOnScreen() {
     let tabs = Array.from(document.getElementsByClassName("tab"));
     let active = document.getElementsByClassName("active")[0];
     if (tabs.indexOf(active) < 10) {
-        scrollTo({
+        window.scrollTo({
             top: 0,
             left: 0,
             behavior: "smooth"
         });
     } else {
         let rect = active.getBoundingClientRect();
-        scrollTo({
+        window.scrollTo({
             top: rect.top + window.pageYOffset - document.documentElement.clientTop,
             left: 0,
             behavior: "smooth"
@@ -23,8 +23,6 @@ function getOnScreen() {
 
 function createTabs(info) {
     let tab = document.createElement("div");
-
-    tab.innerText = info.title + " | " + info.url;
     tab.classList.add("tab");
     if (info.active) {
         tab.classList.add("active");
@@ -32,6 +30,19 @@ function createTabs(info) {
     tab.dataset.id = info.id;
     tab.dataset.name = info.title;
     tab.dataset.url = info.url;
+
+    if (info.favIconUrl !== undefined && info.favIconUrl !== "") {
+        let url = new URL(info.favIconUrl);
+        if (url.protocol.match(/data|https?/)) {
+            let image = document.createElement("img");
+            image.src = info.favIconUrl;
+            tab.appendChild(image);
+        }
+    }
+
+    let name = document.createElement("div");
+    name.innerText = info.title + " | " + info.url;
+    tab.appendChild(name);
 
     tabs_ele.appendChild(tab);
 }
@@ -81,8 +92,8 @@ function switchTab(e) {
             browser.tabs.create({ url: "help.html", active: true });
             window.close();
         } else if (e.key === "s") {
-            browser.runtime.openOptionsPage()
-            window.close()
+            browser.runtime.openOptionsPage();
+            window.close();
         }
     } else if (e.key === "Enter") {
         let filtered = Array.from(document.getElementsByClassName("tab")).filter(
@@ -91,20 +102,43 @@ function switchTab(e) {
         if (filtered.length !== 0) {
             filtered[0].classList.add("active");
             search.blur();
+            search.value = "";
         }
     }
 }
 
 function filter() {
     let tabs = Array.from(document.getElementsByClassName("tab"));
-    let regex = new RegExp(search.value.substring(1));
-    tabs.forEach(item => {
-        if (item.dataset.name.match(regex) || item.dataset.url.match(regex)) {
-            item.classList.remove("hidden");
-        } else {
-            item.classList.add("hidden");
-        }
-    });
+
+    if (!search.value.startsWith("/")) {
+        search.value = "/" + search.value;
+    }
+
+    if (search.dataset.mode === "regex") {
+        let regex = new RegExp(
+            search.value.substring(1),
+            search.dataset.case === "nocase" ? "i" : ""
+        );
+        tabs.forEach(item => {
+            if (item.dataset.name.match(regex) || item.dataset.url.match(regex)) {
+                item.classList.remove("hidden");
+            } else {
+                item.classList.add("hidden");
+            }
+        });
+    } else if (search.dataset.mode === "string") {
+        tabs.forEach(item => {
+            let terms = [search.value.substring(1), item.dataset.url, item.dataset.name];
+            if (search.dataset.case === "nocase") {
+                terms = terms.map(x => x.toLowerCase());
+            }
+            if (terms[1].indexOf(terms[0]) !== -1 || terms[2].indexOf(terms[0]) !== -1) {
+                item.classList.remove("hidden");
+            } else {
+                item.classList.add("hidden");
+            }
+        });
+    }
     if (tabs.some(item => !item.classList.contains("hidden"))) {
         search.classList.remove("invalid");
     } else {
@@ -125,6 +159,8 @@ async function main() {
             item.classList.add("dark")
         );
     }
+    search.dataset.mode = res.searchMode;
+    search.dataset.case = res.caseSensitivity;
 }
 
 document.addEventListener("keydown", switchTab);
